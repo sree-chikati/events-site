a8f7516913bdb1af57765ab265bc777bffbfb854
sree-chikati <srechi453.fusdk12@gmail.com>
2021-02-03 21:38:04 -0800

    Finished TODOs in models.py

--- events_app/models.py
+++ events_app/models.py
@@ -28,9 +28,16 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    guests = db.relationship("Guest", secondary="guest_event", back_populates = "events_attending")
+
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
 
-guest_event_table = None
+guest_event_table = db.Table('guest_event',
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')))
